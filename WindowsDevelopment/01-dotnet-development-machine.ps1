9bb993adcf496792e50a87febab1f56ab69bd1c6
John Allberg <john_allberg@hotmail.com>
2014-05-26 09:24:16 +0200

    Added first script.

---
+++ WindowsDevelopment/01-dotnet-development-machine.ps1
@@ -0,0 +1,34 @@
+#Initial windows configuration
+Update-ExecutionPolicy Unrestricted
+Enable-MicrosoftUpdate
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives-EnableShowFileExtensions
+
+#Runtimes and frameworks
+cinstm DotNet4.0
+cinstm DotNet4.5
+
+#Web servers
+cinstm IIS-WebServerRole -source windowsfeatures
+
+#Web browsers
+cinstm GoogleChrome
+
+#Browser plugins
+cinstm fiddler4
+
+#Visual studio and plugins
+cinstm VisualStudio2013Professional -InstallArguments "WebTools SQL"
+cinstm resharper
+cinstm dotcover
+cinstm stylecop
+Install-ChocolateyVsixPackage "Web Essentials 2013" http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/24/WebEssentials2013.vsix
+
+#VCS
+cinstm gitextensions
+cinstm kdiff3
+
+#Databases
+cinstm SqlServer2012Express
+
+#Install windows updates
+Install-WindowsUpdate -AcceptEula
