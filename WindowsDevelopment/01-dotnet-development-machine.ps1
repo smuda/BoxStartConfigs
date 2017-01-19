91ab24ef80ff911e74f8e97b9c904a8b30ab7d4d
John Allberg <john_allberg@hotmail.com>
2017-01-19 08:46:18 +0100

    Some smaller changes

--- WindowsDevelopment/01-dotnet-development-machine.ps1
+++ WindowsDevelopment/01-dotnet-development-machine.ps1
@@ -7,28 +7,25 @@ Enable-MicrosoftUpdate
 cinstm DotNet4.0
 cinstm DotNet4.5
 
-#Web servers
+# Web servers
 cinstm IIS-WebServerRole -source windowsfeatures
 
-#Web browsers
+# Web browsers
 cinstm GoogleChrome
 
-#Browser plugins
+# Browser plugins
 cinstm fiddler4
 
-#Visual studio and plugins
-cinstm VisualStudio2013Professional -InstallArguments "WebTools SQL"
+# Visual studio and plugins
+cinstm VisualStudio2015Professional -InstallArguments "WebTools SQL"
 cinstm resharper
 cinstm dotcover
 cinstm stylecop
-Install-ChocolateyVsixPackage "Web Essentials 2013" http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/24/WebEssentials2013.vsix
+#Install-ChocolateyVsixPackage "Web Essentials 2015" http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/24/WebEssentials2013.vsix
 
-#VCS
+# VCS
 cinstm gitextensions
 cinstm kdiff3
 
-#Databases
-cinstm SqlServer2012Express
-
-#Install windows updates
+# Install windows updates
 Install-WindowsUpdate -AcceptEula
