f34a9d4e0737b39a37dcad4e7ae493f800162c6b
John Allberg <john_allberg@hotmail.com>
2017-01-19 09:19:05 +0100

    Minor changes

--- WindowsDevelopment/01-dotnet-development-machine.ps1
+++ WindowsDevelopment/01-dotnet-development-machine.ps1
@@ -1,7 +1,6 @@
 #Initial windows configuration
 Update-ExecutionPolicy Unrestricted
-Enable-MicrosoftUpdate
-#Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives-EnableShowFileExtensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives-EnableShowFileExtensions
 
 #Runtimes and frameworks
 cinst DotNet4.0
@@ -17,15 +16,15 @@ cinst GoogleChrome
 cinst fiddler4
 
 # Visual studio and plugins
-cinst VisualStudio2015Professional -InstallArguments "WebTools SQL"
+cinst VisualStudio2015Professional -InstallArguments "WebTools"
 cinst resharper
 cinst dotcover
 cinst stylecop --allow-empty-checksums
 #Install-ChocolateyVsixPackage "Web Essentials 2015" http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/24/WebEssentials2013.vsix
 
+# Jetbrains intelliJ
+cinst intellij
+
 # VCS
 cinst gitextensions --allow-empty-checksums
 cinst kdiff3
-
-# Install windows updates
-Install-WindowsUpdate -AcceptEula
