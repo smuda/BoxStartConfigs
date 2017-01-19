a50023d95597dd1314493e715c5fc1d6ea14b427
John Allberg <john_allberg@hotmail.com>
2017-01-19 08:57:08 +0100

    Use cinst instead of cinstm

--- WindowsDevelopment/01-dotnet-development-machine.ps1
+++ WindowsDevelopment/01-dotnet-development-machine.ps1
@@ -4,28 +4,28 @@ Enable-MicrosoftUpdate
 #Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives-EnableShowFileExtensions
 
 #Runtimes and frameworks
-cinstm DotNet4.0
-cinstm DotNet4.5
+cinst DotNet4.0
+cinst DotNet4.5
 
 # Web servers
-cinstm IIS-WebServerRole -source windowsfeatures
+cinst IIS-WebServerRole -source windowsfeatures
 
 # Web browsers
-cinstm GoogleChrome
+cinst GoogleChrome
 
 # Browser plugins
-cinstm fiddler4
+cinst fiddler4
 
 # Visual studio and plugins
-cinstm VisualStudio2015Professional -InstallArguments "WebTools SQL"
-cinstm resharper
-cinstm dotcover
-cinstm stylecop
+cinst VisualStudio2015Professional -InstallArguments "WebTools SQL"
+cinst resharper
+cinst dotcover
+cinst stylecop
 #Install-ChocolateyVsixPackage "Web Essentials 2015" http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/24/WebEssentials2013.vsix
 
 # VCS
-cinstm gitextensions
-cinstm kdiff3
+cinst gitextensions
+cinst kdiff3
 
 # Install windows updates
 Install-WindowsUpdate -AcceptEula
